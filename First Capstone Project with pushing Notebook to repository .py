bc80c04b5b17c8baba4fb31bfc98773dc1134113
PRANETHSP <68838623+PRANETHSP@users.noreply.github.com>
2020-07-27 11:53:37 +0530

    Add files via upload

---
+++ First Capstone Project with pushing Notebook to repository .py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # This a My first Capstone Project in COURSERA
+# by PRANETH S P
+
+# In[8]:
+
+
+import pandas as pd
+import numpy as np
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
